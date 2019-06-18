dc59f393dd41aee3ebdb50ec40f9e4bc824eda4b
azimex <ajilkhomov@gmail.com>
2019-06-18 17:44:25 -0400

    further implemented UI | setup theme | setup drawer

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,9 @@ void main() {
     (_) {
       runApp(
         new MaterialApp(
-          home: new Scaffold(body: tracked()),
-          theme: ThemeData(
-            fontFamily: 'HurmeGeometricSans1',
-          ),
+          // darkTheme: ThemeData(buttonColor: Color.fromRGBO(51, 153, 255, 1.0)),
+          home: tracked(),
+          theme: ThemeData(),
           debugShowCheckedModeBanner: false,
         ),
       );
@@ -18,34 +17,100 @@ void main() {
 }
 
 class tracked extends StatelessWidget {
-  // TextStyle style = TextStyle(fontFamily: '');
-  final searchField = TextField(
-    decoration: InputDecoration(hintText: '[DOE] or [S/N]'),
-  );
-
-  final searchButton = RaisedButton(
-    onPressed: () => print('pressed [Search]'),
-    child: const Text('Search'),
-  );
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.only(left: 80.0, right: 80.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            searchField,
-            Padding(padding: EdgeInsets.all(10.0)),
-            SizedBox(
-              width: double.infinity, // matches parents width [container]
-              child: RaisedButton(
-                onPressed: () => print('pressed [Search]'),
-                child: const Text('Search'),
+    final logo = Text('tracked', style: style.copyWith(fontSize: 40));
+
+    final searchField = TextField(
+      style: style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(15.0),
+        hintText: '[DOE] or [S/N]',
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final searchButton = Material(
+      color: Color.fromRGBO(51, 153, 255, 1.0),
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(10.0),
+      child: MaterialButton(
+          onPressed: () => print('pressed [Search]'),
+          minWidth: MediaQuery.of(context).size.width, // matches parent width
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          child: Text(
+            'Search',
+            textAlign: TextAlign.left,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+    );
+
+    final drawer = Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            // children: <Widget>[Text('add'), Text('remove')],
+            children: [
+              ListTile(
+                leading: Icon(Icons.arrow_back),
+                onTap: () => Navigator.pop(context),
               ),
+              ListTile(
+                  title: Text('add'),
+                  trailing: Icon(Icons.add),
+                  onTap: () => print('tapped [add]')),
+              ListTile(
+                  title: Text('remove'),
+                  trailing: Icon(Icons.remove),
+                  onTap: () => print('tapped [remove]')),
+            ],
+          ),
+          ListTile(
+              title: Text('settings'),
+              trailing: Icon(Icons.settings),
+              onTap: () => print('tapped [settings]')),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        leading: Builder(
+          builder: (context) => IconButton(
+                icon: new Icon(Icons.dehaze, color: Colors.black),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+        ),
+      ),
+      drawer: Drawer(child: drawer),
+      body: Center(
+        child: Container(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 60.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                logo,
+                SizedBox(height: 30.0),
+                searchField,
+                SizedBox(height: 20.0),
+                searchButton,
+                SizedBox(height: 50.0),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
