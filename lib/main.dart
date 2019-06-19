f36a9e0647c77f32161e076c10a49beb4fa67215
azimex <ajilkhomov@gmail.com>
2019-06-18 21:06:55 -0400

    implemented dark mode | design: revision 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+/* TODO: Dynamic Theme Selection */
+/* BUG: InkWell Effect on Drawer elements not working */
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -6,9 +9,17 @@ void main() {
     (_) {
       runApp(
         new MaterialApp(
-          // darkTheme: ThemeData(buttonColor: Color.fromRGBO(51, 153, 255, 1.0)),
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            hintColor: Colors.grey.shade500,
+            backgroundColor: Colors.black,
+            splashColor: Colors.white,
+            accentColor: Color.fromRGBO(51, 153, 255, 1.0),
+            buttonColor: Color.fromRGBO(51, 153, 255, 1.0),
+          ),
           home: tracked(),
-          theme: ThemeData(),
+          // theme: ThemeData(),
+          // theme: ThemeData.dark(),
           debugShowCheckedModeBanner: false,
         ),
       );
@@ -24,18 +35,24 @@ class tracked extends StatelessWidget {
     final logo = Text('tracked', style: style.copyWith(fontSize: 40));
 
     final searchField = TextField(
-      style: style,
+      // style: style,
       decoration: InputDecoration(
         contentPadding: EdgeInsets.all(15.0),
         hintText: '[DOE] or [S/N]',
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(color: Theme.of(context).primaryColor)),
+        suffixIcon: IconButton(
+          // icon: Icon(Icons.center_focus_weak, size: 30.0),
+          // icon: Icon(Icons.crop_free, size: 30.0, ),
+          icon: Icon(Icons.filter_center_focus, size: 30.0),
+          onPressed: () => print('tapped [scan]'),
         ),
       ),
     );
 
     final searchButton = Material(
-      color: Color.fromRGBO(51, 153, 255, 1.0),
+      color: Color.fromRGBO(51, 153, 255, 1.0), // have MaterialApp track this
       elevation: 2.0,
       borderRadius: BorderRadius.circular(10.0),
       child: MaterialButton(
@@ -50,45 +67,51 @@ class tracked extends StatelessWidget {
           )),
     );
 
-    final drawer = Padding(
-      padding: EdgeInsets.all(10.0),
+    final drawer = Container(
+      padding: EdgeInsets.fromLTRB(10, 30, 0, 0),
+      color: Theme.of(context).primaryColor,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            // children: <Widget>[Text('add'), Text('remove')],
             children: [
               ListTile(
-                leading: Icon(Icons.arrow_back),
+                leading: Icon(Icons.arrow_back,
+                    color: Theme.of(context).buttonColor),
                 onTap: () => Navigator.pop(context),
               ),
               ListTile(
-                  title: Text('add'),
-                  trailing: Icon(Icons.add),
-                  onTap: () => print('tapped [add]')),
+                  title: Text('Export', style: style),
+                  trailing: Icon(Icons.vertical_align_top,
+                      color: Theme.of(context).buttonColor),
+                  onTap: () => print('tapped [export]')),
               ListTile(
-                  title: Text('remove'),
-                  trailing: Icon(Icons.remove),
-                  onTap: () => print('tapped [remove]')),
+                  title: Text('Import', style: style),
+                  trailing: Icon(Icons.vertical_align_bottom,
+                      color: Theme.of(context).buttonColor),
+                  onTap: () => print('tapped [import]')),
             ],
           ),
           ListTile(
-              title: Text('settings'),
-              trailing: Icon(Icons.settings),
+              title: Text('Settings', style: style),
+              trailing:
+                  Icon(Icons.settings, color: Theme.of(context).buttonColor),
               onTap: () => print('tapped [settings]')),
         ],
       ),
     );
 
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         elevation: 0.0,
         backgroundColor: Colors.transparent,
         leading: Builder(
           builder: (context) => IconButton(
-                icon: new Icon(Icons.dehaze, color: Colors.black),
+                icon: new Icon(Icons.dehaze,
+                    color: Theme.of(context).buttonColor),
                 onPressed: () => Scaffold.of(context).openDrawer(),
               ),
         ),
@@ -107,7 +130,7 @@ class tracked extends StatelessWidget {
                 searchField,
                 SizedBox(height: 20.0),
                 searchButton,
-                SizedBox(height: 50.0),
+                SizedBox(height: 100.0),
               ],
             ),
           ),
