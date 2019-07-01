94b8b0b37f6fb479b7a8b8c82d79caf74b392c92
ajilk <ajilkhomov@gmail.com>
2019-07-01 14:55:26 -0400

    Remove gesture detection in AssetPage (slide right to go back)

--- lib/AssetPage.dart
+++ lib/AssetPage.dart
@@ -22,32 +22,22 @@ class AssetPage extends StatelessWidget {
         ],
       ),
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Stack(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15.0),
-            child: ListView(
-              children: [
-                Field(text: 'Asset Tag'),
-                Field(text: 'Serial Number'),
-                Field(text: 'Location', keyboardType: TextInputType.number),
-                Field(text: 'Manufacturer'),
-                Field(text: 'Model'),
-                Field(
-                  text: 'Description',
-                  keyboardType: TextInputType.multiline,
-                ),
-                SizedBox(height: 24.0),
-              ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        child: ListView(
+          children: [
+            Field(text: 'Asset Tag'),
+            Field(text: 'Serial Number'),
+            Field(text: 'Location', keyboardType: TextInputType.number),
+            Field(text: 'Manufacturer'),
+            Field(text: 'Model'),
+            Field(
+              text: 'Description',
+              keyboardType: TextInputType.multiline,
             ),
-          ),
-          GestureDetector(
-            child: Container(width: 100.0),
-            onHorizontalDragEnd: (details) {
-              if (details.primaryVelocity > 0) Navigator.pop(context);
-            },
-          ),
-        ],
+            SizedBox(height: 24.0),
+          ],
+        ),
       ),
     );
   }
